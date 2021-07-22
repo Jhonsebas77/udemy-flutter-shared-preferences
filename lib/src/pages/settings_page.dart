5ea7b63895721cb04a3b266b98a4e212484a2d61
Sebastian Otalora <sebastianotalora@tyba.com>
2021-07-21 23:47:03 -0500

    Add the default values and controllers

--- lib/src/pages/settings_page.dart
+++ lib/src/pages/settings_page.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences_udemy/src/widgets/widgets.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   static final String routeName = 'settings';
   const SettingsPage({Key? key}) : super(key: key);
+
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  bool _secondaryColor = false;
+  int _gender = 1;
+  String _name = 'User Name';
+
+  late TextEditingController _textEditingController;
+
+  @override
+  void initState() {
+    super.initState();
+    _textEditingController = TextEditingController(text: _name);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +29,61 @@ class SettingsPage extends StatelessWidget {
         title: Text('Ajustes'),
       ),
       drawer: MenuDrawer(),
-      body: Center(
-        child: Text(
-          'Ajustes Page',
-        ),
+      body: ListView(
+        children: [
+          Container(
+            padding: EdgeInsets.all(5),
+            child: Text(
+              'Settings',
+              style: TextStyle(
+                fontSize: 45,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ),
+          Divider(),
+          SwitchListTile(
+            value: _secondaryColor,
+            onChanged: (value) {
+              setState(() {
+                _secondaryColor = value;
+              });
+            },
+            title: Text('Color Secundario'),
+          ),
+          RadioListTile(
+            value: 1,
+            title: Text('Masculino'),
+            groupValue: _gender,
+            onChanged: (value) {
+              setState(() {
+                _gender = value as int;
+              });
+            },
+          ),
+          RadioListTile(
+            value: 2,
+            title: Text('Femenino'),
+            groupValue: _gender,
+            onChanged: (value) {
+              setState(() {
+                _gender = value as int;
+              });
+            },
+          ),
+          Divider(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: TextField(
+              controller: _textEditingController,
+              decoration: InputDecoration(
+                labelText: 'Nombre',
+                helperText: 'Nombre de Usuario',
+              ),
+              onChanged: (value) {},
+            ),
+          ),
+        ],
       ),
     );
   }
